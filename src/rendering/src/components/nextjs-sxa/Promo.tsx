02b68351eb2c22bc276753744155287ef7d8cbe3
Brandon Bruno <brab@sitecore.net>
2022-11-15 17:02:34 -0500

    Removed unnecessary promo-text class.

--- src/rendering/src/components/nextjs-sxa/Promo.tsx
+++ src/rendering/src/components/nextjs-sxa/Promo.tsx
@@ -49,7 +49,7 @@ export const Default = (props: PromoProps): JSX.Element => {
               <Text className="promo-text" field={props.fields.PromoText} />
             </div>
             <div className="field-promotext2">
-              <JssRichText className="promo-text" field={props.fields.PromoText2} />
+              <JssRichText field={props.fields.PromoText2} />
             </div>
             {/* END CUSTOMIZATION */}
             <div className="field-promolink">
